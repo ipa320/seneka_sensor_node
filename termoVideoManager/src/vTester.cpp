03669dd5ffb6c60f9d157750e1faa741aabdfb1b
ipa-cmm-jg <Johannes.Goth@ipa.fraunhofer.de>
2013-05-14 15:40:30 +0200

    repo cleaning

--- termoVideoManager/src/vTester.cpp
+++ termoVideoManager/src/vTester.cpp
@@ -1,9 +1,61 @@
-/*
- * vTester.cpp
+/*!
+ *****************************************************************
+ * \file
  *
- *  Created on: 23.04.2013
- *      Author: Johannes Goth (cmm-jg)
- */
+ * \note
+ *   Copyright (c) 2012 \n
+ *   Fraunhofer Institute for Manufacturing Engineering
+ *   and Automation (IPA) \n\n
+ *
+ *****************************************************************
+ *
+ * \note
+ *   Project name: SENEKA
+ * \note
+ *   ROS stack name: SENEKA
+ * \note
+ *   ROS package name: termoVideoManager
+ *
+ * \author
+ *   Author: Johannes Goth (cmm-jg)
+ * \author
+ *   Supervised by: Christophe Maufroy (cmm)
+ *
+ * \date Date of creation: 23.04.2013
+ *
+ * \brief
+ *   vTester.cpp
+ *
+ *****************************************************************
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     - Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer. \n
+ *     - Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution. \n
+ *     - Neither the name of the Fraunhofer Institute for Manufacturing
+ *       Engineering and Automation (IPA) nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission. \n
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License LGPL as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License LGPL for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License LGPL along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ ****************************************************************/
 
 #include "ros/ros.h"
 #include "termoVideoManager/getVideo.h"
@@ -44,15 +96,25 @@ int main(int argc, char **argv)
 		// connect to server
 		client.call(videoService);
 
-		// user feedback
-		if((int)videoService.response.releasedVideo == 1)
-			ROS_INFO("Creating video ...");
-		else if((int)videoService.response.releasedVideo == -1)
-			ROS_ERROR("Video recorder is busy !!");
-		else if((int)videoService.response.releasedVideo == -2)
-			ROS_ERROR("NO complete video available !!");
-		else
-			ROS_ERROR("Connection to Server failed");
+		// feedback by frameManager
+		switch((int)videoService.response.releasedVideo){
+
+		case 1 :	ROS_INFO("Creating video ...");
+					break;
+
+		case -1:	ROS_ERROR("Video recorder is busy !!");
+					break;
+
+		case -2:	ROS_ERROR("NO complete video available !!");
+					break;
+
+		case -3:	ROS_ERROR("VideoOnDemand is in state LIVE_STREAM not available! "
+					"Cancel state LIVE_STREAM to switch to state ON_DEMAND!");
+					break;
+
+		default:	ROS_ERROR("Connection to Server failed or unknown command!");
+					break;
+		}
 		break;
 	case 2:
 		ROS_INFO("MODE SnapShot");
