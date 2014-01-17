03f68507035c420aa1b76f89ca7cd98c29eeebcb
Thorsten Andreas Kannacher <thorsten.andreas.kannacher@ipa.fraunhofer.de>
2014-01-17 12:03:02 +0100

    created service server and service client in laser_scan node

--- seneka_laser_scan/src/seneka_laser_scan.cpp
+++ seneka_laser_scan/src/seneka_laser_scan.cpp
@@ -1,26 +1,40 @@
 #include <ros/ros.h>
+#include <std_srvs/Empty.h>
 #include <laser_assembler/AssembleScans.h>
 
-int main(int argc, char **argv)
+bool response(std_srvs::Empty::Request& request, std_srvs::Empty::Response& response)
 {
-  ros::init(argc, argv, "laser_scan");
-  ros::NodeHandle nh;
+  ROS_INFO("Scanning environment...\n");
 
+  ros::NodeHandle nh2;
   ros::service::waitForService("assemble_scans");
-  ros::ServiceClient client = nh.serviceClient<laser_assembler::AssembleScans>("assemble_scans");
+  ros::ServiceClient client = nh2.serviceClient<laser_assembler::AssembleScans>("assemble_scans");
+ 
   laser_assembler::AssembleScans srv;
-
   srv.request.begin = ros::Time(0,0);
-  srv.request.end   = ros::Time::now();
+  srv.request.end = ros::Time::now();
 
   if (client.call(srv))
   {
-    ROS_INFO("Got cloud with %u points\n", srv.response.cloud.points.size());
+    ROS_INFO("Service call succeeded\n");
   }
   else
   {
     ROS_ERROR("Service call failed\n");
   }
+
+  return true;
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "laser_scan");
+
+  ros::NodeHandle nh1;
+  ros::ServiceServer service = nh1.advertiseService("laser_scan", response);
+  ROS_INFO("Ready to scan environment\n");
+
+  ros::spin();
 
   return 0;
 }
