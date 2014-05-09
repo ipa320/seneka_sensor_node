3598d94d9f4b99ae0696ab222038089267783d4c
Thorsten Andreas Kannacher <Thorsten.Andreas.Kannacher@ipa.fraunhofer.de>
2014-05-09 17:08:20 +0200

    errors fixed

--- seneka_dgps/ros/src/seneka_dgps_node.cpp
+++ seneka_dgps/ros/src/seneka_dgps_node.cpp
@@ -98,7 +98,7 @@ int main(int argc, char** argv) {
 
         }
 
-        count++;6
+        count++;
 
         // in case of success, waiting for DPGS to get ready;
         // in case of an error, waiting before retry to connect;
@@ -140,8 +140,6 @@ int main(int argc, char** argv) {
         /**************************************************/
         /**************************************************/
         /**************************************************/
-
-    }
 
     return 0;
 
