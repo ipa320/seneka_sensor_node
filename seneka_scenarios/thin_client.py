8675f8386e0dd52be6992508b8bb30999f1b8351
sensornode2 <robot@sensornode2.(none)>
2014-10-15 06:58:14 +0200

    upated thin client

--- seneka_scenarios/thin_client.py
+++ seneka_scenarios/thin_client.py
@@ -1,10 +1,12 @@
-from json import dumps, loads
+from json import dumps
+import json, sys
 #https://github.com/Lawouach/WebSocket-for-Python
 from ws4py.client.threadedclient import WebSocketClient
 import time
 
 class GetLoggersClient(WebSocketClient):
      #interface
+     on_result = None
 
      def extend(self):
          self.call("/extend")
@@ -14,6 +16,10 @@ class GetLoggersClient(WebSocketClient):
 
      def scan(self):
          self.call("/scan")
+         
+     def move_to(self, pos):
+         msg = {'op': 'publish', 'topic': '/move_turret', 'msg': {'data': pos}}
+         self.send(dumps(msg))
 
      #internal
      def call(self, srv):
@@ -22,18 +28,34 @@ class GetLoggersClient(WebSocketClient):
 
      def opened(self):
          print "Connection opened..."
+         msg = {'op': 'subscribe', 'topic': "/bridge_response"}
+         self.send(dumps(msg))
 
      def closed(self, code, reason=None):
          print code, reason
          self.connect()
 
      def received_message(self, m):
-         print "received", m
-         print(loads(m))
+         try:
+                  msg = json.loads(str(m))
+                  if not 'topic' in msg or msg['topic']!='/bridge_response': return
+         
+                  ar = msg['msg']['data'].split(" ")
+                  if len(ar)<2: return
+                  req = ar[0]
+                  success = bool(ar[1])
+                  if self.on_result!=None:
+                           self.on_result(req,success)
+         except:
+                  print "error", sys.exc_info()[0]
 
+def sample_handler(req, success):
+	print req, success
+	
 if __name__=="__main__":
      try:
          ws = GetLoggersClient('ws://127.0.0.1:9090/')	#adjust here
+         ws.on_result = sample_handler
          ws.connect()
          while True: time.sleep(1)
      except KeyboardInterrupt:
